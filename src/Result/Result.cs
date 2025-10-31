c9f5d3e8e17e7cc37134fe446745dd6fdd521fb6
dae-ne <rafalcz29@gmail.com>
2025-10-31 23:33:49 +0100

    Add switch method to result

--- src/Result/Result.cs
+++ src/Result/Result.cs
@@ -9,14 +9,25 @@ public sealed class Result<TData> : ResultBase<TData>
 
     private Result(ErrorBase error) : base(error) { }
 
-    public static implicit operator Result<TData>(TData data) => new(data);
+    public void Switch(Action<TData> onSuccess, Action<ErrorBase> onFailure)
+    {
+        if (IsSuccess)
+        {
+            onSuccess(Data!);
+            return;
+        }
 
-    public static implicit operator Result<TData>(ErrorBase error) => new(error);
+        onFailure(Error!);
+    }
 
     public TResult Match<TResult>(
         Func<TData, TResult> onSuccess,
         Func<ErrorBase, TResult> onFailure)
         => IsSuccess ? onSuccess(Data!) : onFailure(Error!);
+
+    public static implicit operator Result<TData>(TData data) => new(data);
+
+    public static implicit operator Result<TData>(ErrorBase error) => new(error);
 }
 
 public sealed class Result : ResultBase<Unit>
@@ -25,12 +36,23 @@ public sealed class Result : ResultBase<Unit>
 
     private Result(ErrorBase error) : base(error) { }
 
-    public static implicit operator Result(Unit value) => new(value);
+    public void Switch(Action onSuccess, Action<ErrorBase> onFailure)
+    {
+        if (IsSuccess)
+        {
+            onSuccess();
+            return;
+        }
 
-    public static implicit operator Result(ErrorBase error) => new(error);
+        onFailure(Error!);
+    }
 
     public TResult Match<TResult>(
         Func<TResult> onSuccess,
         Func<ErrorBase, TResult> onFailure)
         => IsSuccess ? onSuccess() : onFailure(Error!);
+
+    public static implicit operator Result(Unit value) => new(value);
+
+    public static implicit operator Result(ErrorBase error) => new(error);
 }
