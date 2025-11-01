e7c7157c6240e5f7d14a106a7a87389505ed365f
dae-ne <rafalcz29@gmail.com>
2025-11-01 15:05:41 +0100

    Use fully qualified display format in generated code

---
+++ src/Result.Generator/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace PxBunny.Result;
+
+internal static class Extensions
+{
+    public static string GetName(this ClassDeclarationSyntax node)
+    {
+        return node.Identifier.Text;
+    }
+
+    public static string GetFullName(this ClassDeclarationSyntax node, SemanticModel model)
+    {
+        var symbol = model.GetDeclaredSymbol(node);
+
+        return symbol is null
+            ? node.GetName()
+            : symbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+    }
+}
